e7ab1bd9276bd17257bb0e65f49b99aecaeb2da2
Chris Rudge <chrisrudge2002@gmail.com>
2017-01-22 14:35:37 +0000

    Add private chat rooms

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,14 @@ const name = getQueryVariable('name') || 'Anonymous';
 const room = getQueryVariable('room');
 const socket = io();
 
+jQuery('.room-title').text(room);
+
 socket.on('connect', function() {
 	console.log('Connected to socket.io server!');
+	socket.emit('joinRoom', {
+		name,
+		room
+	});
 });
 
 socket.on('message', function(message) {
