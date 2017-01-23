36e5a199f0d4f514177b986e2863a92acd920798
Chris Rudge <chrisrudge2002@gmail.com>
2017-01-23 18:49:25 +0000

    Style chat page

--- public/js/app.js
+++ public/js/app.js
@@ -14,13 +14,16 @@ socket.on('connect', function() {
 
 socket.on('message', function(message) {
 	const momentTimestamp = moment.utc(message.timestamp);
-	const $message = jQuery('.messages')
+	const $messages = jQuery('.messages')
+	const $message = jQuery('<li class="list-group-item"></li>')
+	const messageText = `${message.name} ${momentTimestamp.local().format('h:mm a')}`;
 
 	console.log('New message:');
 	console.log(message.text);
 
-	$message.append(`<p><strong>${message.name} ${momentTimestamp.local().format('h:mm a')}</strong></p>`);
+	$message.append(`<p><strong>${messageText}</strong></p>`);
 	$message.append(`<p>${message.text}</p>`);
+	$messages.append($message);	
 });
 
 // Handles submitting of new message
